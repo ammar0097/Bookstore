15562a293c58eaac38ae71a051e410c54bc368aa
Ammar <ammarferchichi7@gmail.com>
2022-09-16 13:21:08 +0100

    Add project files.

---
+++ WebApplication8/Models/Repositories/BookRepository.cs
@@ -0,0 +1,54 @@
+﻿namespace WebApplication8.Models.Repositories
+{
+    public class BookRepository : IBookstoreRepository<Book>
+    {
+        List<Book> books;
+        public BookRepository()
+        {
+            books = new List<Book>() {
+            new Book
+            {
+                Id = 1, Title = "c# prog", Description = "no description" 
+            },
+            new Book
+            {
+                Id = 2, Title = "php prog", Description = "no description"
+            },
+            new Book
+            {
+                Id = 3, Title = "java prog", Description = "no description"
+            }
+            };
+        }
+        public void Add(Book entity)
+        {
+            books.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var book = FindById(id);
+            books.Remove(book);
+        }
+
+        public Book FindById(int id)
+        {
+            var book = books.SingleOrDefault(b => b.Id == id);
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+            return books;
+        }
+
+        public void Update(int id,Book entity)
+        {
+            var book = FindById(id);
+            book.Title = entity.Title;
+            book.Description = entity.Description;  
+            book.Author = entity.Author; 
+
+        }
+    }
+}
---
+++ WebApplication8/Models/Repositories/IBookstoreRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication8.Models.Repositories
+{
+    public interface IBookstoreRepository<TEntity>
+    {
+        IList<TEntity> List();
+        TEntity FindById(int id);
+        void Add(TEntity entity);
+        void Update(int id ,TEntity entity);
+        void Delete(int id);
+
+
+
+    }
+}
