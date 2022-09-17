7ee7fb84f453a1abed15600321782ba1c5e1056a
Ammar <ammarferchichi7@gmail.com>
2022-09-17 13:18:27 +0100

    Author controller and views

--- WebApplication8/Program.cs
+++ WebApplication8/Program.cs
@@ -1,7 +1,14 @@
+using WebApplication8.Models;
+using WebApplication8.Models.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IBookstoreRepository<Author>, AuthorRepository>();
+builder.Services.AddSingleton<IBookstoreRepository<Book>, BookRepository>();
+
+
 
 var app = builder.Build();
 
